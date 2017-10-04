37338e10f84bd68a44b46aff978c94b0ec8f0542
bitchiko tchelidze <bubachelidze1@gmail.com>
2017-10-04 15:25:42 +0400

    Added AspMvcViewLocationFormat to support resharper View intellisense and navigation.

---
+++ Asp.Net.Screaming.Demo/AssemblyAttributes.cs
@@ -0,0 +1,5 @@
+﻿using JetBrains.Annotations;
+
+[assembly: AspMvcViewLocationFormat(@"~\Features\HomePage\Views\{1}\{0}.cshtml")]
+[assembly: AspMvcViewLocationFormat(@"~\Features\Membership\Views\{1}\{0}.cshtml")]
+[assembly: AspMvcViewLocationFormat(@"~\Features\Shared\Views\{0}.cshtml")]
--- Asp.Net.Screaming/obj/Debug/netcoreapp2.0/Asp.Net.Core.Screaming.AssemblyInfo.cs
+++ Asp.Net.Screaming/obj/Debug/netcoreapp2.0/Asp.Net.Core.Screaming.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.Net.Core.Screaming")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0")]
